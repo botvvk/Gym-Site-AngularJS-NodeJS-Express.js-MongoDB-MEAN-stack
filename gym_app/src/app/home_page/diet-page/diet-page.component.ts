f6b03263c3f8fd0a7e7772bbeb01769098705cf6
Vivek Tiwari <vivekt077@gmail.com>
2022-10-31 16:11:53 +0530

    update code with diet plan page

--- gym_app/src/app/home_page/diet-page/diet-page.component.ts
+++ gym_app/src/app/home_page/diet-page/diet-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { dietPlan, lowCarbDietPlans } from './diet-plans-data';
 
 @Component({
   selector: 'app-diet-page',
@@ -6,6 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./diet-page.component.css']
 })
 export class DietPageComponent implements OnInit {
+
+  tabArray = [
+    { id: 1, label: "BreakFast" },
+    { id: 2, label: "Snack" },
+    { id: 3, label: "Lunch" },
+    { id: 4, label: "Snack" },
+    { id: 5, label: "Dinner" }
+
+  ]
+
+  dietPlans: Array<dietPlan> = lowCarbDietPlans
 
   constructor() { }
 
---
+++ gym_app/src/app/home_page/diet-page/diet-plans-data.ts
@@ -0,0 +1,254 @@
+
+
+export const lowCarbDietPlans: Array<dietPlan> = [
+    {
+        id: 5,
+        imageLink: "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F44%2F2019%2F09%2F13%2F7104329.jpg&w=272&h=272&c=sc&poi=face&q=60",
+        name: " Creamy Chicken Noodle Soup with Rotisserie Chicken ",
+        description: " This creamy rotisserie chicken soup has a delicious chicken pot pie feel to it. We call for rotisserie chicken to streamline your prep time--look for a nice big one with lots of breast meat on it. Pair this comforting and easy soup recipe with a green salad, or, if you're really hungry, a grilled cheese sandwich. ",
+        type: {
+            typeName: "dinner",
+            id: 5
+        }
+    },
+    {
+        id: 2,
+        imageLink: "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F44%2F2021%2F09%2F23%2Fspaghetti-and-spinach-with-sun-dried-tomato-cream-sauce.jpg&w=272&h=272&c=sc&poi=%5B1040%2C1140%5D&q=60&rect=0%2C0%2C2000%2C2000",
+        name: "Spaghetti & Spinach with Sun-Dried Tomato Cream Sauce (tk final title)",
+        description: " To achieve flavor quickly in this sun-dried tomato pasta recipe, we use the oil from sun-dried tomatoes to create the base of the cream sauce. Meanwhile, the residual heat of the pasta wilts the spinach in record time for a fast weeknight dinner. ",
+        type: {
+            typeName: "dinner",
+            id: 5
+        }
+    }, {
+        id: 3,
+        imageLink: "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F44%2F2019%2F10%2F04%2F7115885.jpg&w=272&h=272&c=sc&poi=face&q=60",
+        name: "Chicken Cutlets with Sundried Tomato Cream Sauce",
+        description: " Though a chicken cutlet may be a chicken breast cut in half, this recipe shows how to make chicken cutlets with double the deliciousness. A jar of sun-dried tomatoes does double duty for this healthy dinner idea. The flavorful oil they're packed in is used to sauté the chicken, and the tomatoes go into the cream sauce. ",
+        type: {
+            typeName: "dinner",
+            id: 5
+        }
+    }, {
+        id: 4,
+        imageLink: "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F44%2F2016%2F12%2F27122556%2F52380891.jpg&w=272&h=272&c=sc&poi=face&q=60",
+        name: " American Goulash ",
+        description: " American goulash, also known as old-fashioned goulash, is the perfect economical family meal. The pasta cooks right in the sauce, so this satisfying dish can cook in just one pot. ",
+        type: {
+            typeName: "dinner",
+            id: 5
+        }
+    }, {
+        id: 5,
+        imageLink: "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F44%2F2020%2F10%2F09%2Feasy-italian-wedding-soup-960x960-1.jpg&w=272&h=272&c=sc&poi=face&q=60",
+        name: " Minestra Maritata (Italian Wedding Soup) ",
+        description: " Forget the marble-size meatballs you find in many versions of this Italian Wedding Soup. In this easy recipe, they're full-size, full-flavored and plenty filling. ",
+        type: {
+            typeName: "dinner",
+            id: 5
+        }
+    }, {
+        id: 6,
+        imageLink: "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F44%2F2020%2F01%2F05%2F7582923.jpg&w=272&h=272&c=sc&poi=face&q=60",
+        name: " Spinach & Mushroom Quiche ",
+        description: " This healthy vegetarian quiche recipe is as simple as it gets. It's a quiche without the fussy crust! It's filled with sweet wild mushrooms and savory Gruyère cheese. Enjoy it for breakfast or brunch, or serve it with a light salad for lunch. ",
+        type: {
+            typeName: "breakfast",
+            id: 1
+        }
+    }, {
+        id: 7,
+        imageLink: "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F44%2F2018%2F10%2F22%2F5888434.jpg&w=272&h=272&c=sc&poi=face&q=60",
+        name: " Cinnamon-Roll Overnight Oats ",
+        description: " It takes just minutes to assemble this healthy no-cook breakfast, and you'll have meal-prepped grab-and-go breakfasts on hand for the rest of the week. Top these delicious vegan oats—inspired by classic cinnamon bun flavors—with fresh or frozen fruit and your favorite nuts and seeds. ",
+        type: {
+            typeName: "breakfast",
+            id: 1
+        }
+    },
+    {
+        id: 8,
+        imageLink: "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F44%2F2021%2F05%2F13%2FBaked-Banana-Nut-Oatmeal-Cups.jpg&w=272&h=272&c=sc&poi=face&q=60",
+        name: " Baked Banana-Nut Oatmeal Cups ",
+        description: " Muffins meet oatmeal in these moist and tasty grab-and-go oatmeal cups. ",
+        type: {
+            typeName: "breakfast",
+            id: 1
+        }
+    },
+    {
+        id: 9,
+        imageLink: "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F44%2F2022%2F04%2F15%2Fspinach-mushroom-egg-casserole.jpg&w=272&h=272&c=sc&poi=face&q=60&rect=0%2C0%2C2000%2C2000",
+        name: " Spinach, Mushroom & Egg Casserole ",
+        description: " This delightful spinach, mushroom and egg casserole is layered with earthy cooked mushrooms and baby spinach, fluffy eggs and nutty cave-aged Gruyère that deepens the flavor. Serve this easy casserole for breakfast, brunch or even dinner with a green salad on the side. ",
+        type: {
+            typeName: "breakfast",
+            id: 1
+        }
+    },
+    {
+        id: 10,
+        imageLink: "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F44%2F2022%2F10%2F24%2FCinnamonRollEatingWell-4.jpg&w=272&h=272&c=sc&poi=%5B980%2C980%5D&q=60&rect=417%2C454%2C1559%2C1596",
+        name: " Cinnamon Roll for One ",
+        description: " Enjoy a delicious, homemade cinnamon roll with this make-ahead recipe. This recipe yields eight rolls, so you can make the rolls in advance and store them in the freezer, then just pop one out when you're ready to bake. These rolls are made with whole-grain flour and have less sugar than most cinnamon roll recipes, but they're packed with flavor and just as fun to eat. ",
+        type: {
+            typeName: "breakfast",
+            id: 1
+        }
+    },
+    {
+        id: 11,
+        imageLink: "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F44%2F2020%2F10%2F09%2Feasy-italian-wedding-soup-960x960-1.jpg&w=272&h=272&c=sc&poi=face&q=60",
+        name: " Minestra Maritata (Italian Wedding Soup) ",
+        description: " Forget the marble-size meatballs you find in many versions of this Italian Wedding Soup. In this easy recipe, they're full-size, full-flavored and plenty filling. ",
+        type: {
+            typeName: "lunch",
+            id: 3
+        }
+    },
+    {
+        id: 12,
+        imageLink: "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F44%2F2020%2F01%2F05%2F7582923.jpg&w=272&h=272&c=sc&poi=face&q=60",
+        name: " Spinach & Mushroom Quiche ",
+        description: " This healthy vegetarian quiche recipe is as simple as it gets. It's a quiche without the fussy crust! It's filled with sweet wild mushrooms and savory Gruyère cheese. Enjoy it for breakfast or brunch, or serve it with a light salad for lunch. ",
+        type: {
+            typeName: "lunch",
+            id: 3
+        }
+    }, {
+        id: 13,
+        imageLink: "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F44%2F2022%2F03%2F01%2Fcucumber-sandwich.jpg&w=272&h=272&c=sc&poi=%5B920%2C780%5D&q=60&rect=0%2C0%2C2000%2C2000",
+        name: " Cucumber Sandwich ",
+        description: " This creamy, crunchy cucumber sandwich recipe strikes a lovely balance between decadent and light. The cream cheese-yogurt spread complements the crisp refreshing cucumber while the hearty flavor and texture of the whole-wheat bread holds everything together. Removing the crusts makes it more delicate than your average sandwich. ",
+        type: {
+            typeName: "lunch",
+            id: 3
+        }
+    }, {
+        id: 14,
+        imageLink: "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F44%2F2020%2F08%2F12%2F8432277.jpg&w=272&h=272&c=sc&poi=face&q=60",
+        name: " Stuffed Cabbage Soup ",
+        description: " Like stuffed cabbage? You'll love this easy stuffed cabbage soup. It's got all of the classic flavors of stuffed cabbage without all the fuss of stuffing. Plus, you can make this warming soup ahead of time or freeze it for later for a quick dinner or lunch. ",
+        type: {
+            typeName: "lunch",
+            id: 3
+        }
+    }, {
+        id: 15,
+        imageLink: "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F44%2F2022%2F05%2F03%2Fcarrot-soup.jpg&w=272&h=272&c=sc&poi=%5B960%2C940%5D&q=60&rect=0%2C0%2C2000%2C2000",
+        name: " Carrot Soup ",
+        description: " This easy carrot soup recipe is a great way to use up a bag of carrots that were forgotten in your produce drawer. The carrots cook together with aromatics like onions, garlic and fresh herbs before being puréed into a silky smooth soup that's delicious for dinner or packed up for lunch. ",
+        type: {
+            typeName: "lunch",
+            id: 3
+        }
+    },
+    {
+        id: 16,
+        imageLink: "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F44%2F2021%2F08%2F30%2Fapple-pie-energy-balls.jpg&w=272&h=272&c=sc&poi=%5B1040%2C800%5D&q=60&rect=0%2C1%2C2000%2C2001",
+        name: " Apple Pie Energy Balls ",
+        description: " These no-bake Apple Pie Energy Balls come together easily and will give your body the energy it needs to power through the afternoon. ",
+        type: {
+            typeName: "snack",
+            id: 2
+        }
+    },
+    {
+        id: 17,
+        imageLink: "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F44%2F2020%2F09%2F22%2Fpeanut-butter-energy-balls.jpeg&w=272&h=272&c=sc&poi=face&q=60",
+        name: " Peanut Butter Energy Balls ",
+        description: " These healthy peanut butter and chocolate energy balls deliver a mixture of simple and complex carbohydrates to help fuel you up whenever you need a little boost. ",
+        type: {
+            typeName: "snack",
+            id: 2
+        }
+    }, {
+        id: 18,
+        imageLink: "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F44%2F2022%2F08%2F03%2FBreakfast-Peanut-Butter-Chocolate-Chip-Oatmeal-Cakes-v2-1x1-1.jpg&w=272&h=272&c=sc&poi=%5B980%2C960%5D&q=60&rect=163%2C276%2C1763%2C1876",
+        name: " Breakfast Peanut Butter-Chocolate Chip Oatmeal Cakes ",
+        description: " Peanut butter is the star ingredient in these oatmeal cakes, providing not only flavor, but a boost of plant-based protein too. Hiding a bit in the center of each muffin is a fun way to ensure that peanut butter makes it into every bite. ",
+        type: {
+            typeName: "snack",
+            id: 2
+        }
+    }, {
+        id: 19,
+        imageLink: "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F44%2F2020%2F08%2F14%2F8440286.jpg&w=272&h=272&c=sc&poi=face&q=60",
+        name: " Pumpkin-Oatmeal Muffins ",
+        description: " These healthy pumpkin-oatmeal muffins will get you in the mood for fall. Sprinkling the muffins with pecans adds a nutty crunch to every bite. Serve them for breakfast or as a grab-and-go snack. ",
+        type: {
+            typeName: "snack",
+            id: 2
+        }
+    }, {
+        id: 20,
+        imageLink: "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F44%2F2021%2F07%2F09%2Froasted-butternut-squash-seeds.jpg&w=272&h=272&c=sc&poi=%5B900%2C960%5D&q=60&rect=0%2C0%2C2000%2C2000",
+        name: " Roasted Butternut Squash Seeds ",
+        description: " Don't throw away your butternut squash seeds; roast them instead! Roasted butternut squash seeds make a great snack or salad topper. Enjoy them plain or jazzed up with one of the variations below. ",
+        type: {
+            typeName: "snack",
+            id: 2
+        }
+    },
+    {
+        id: 21,
+        imageLink: "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F44%2F2019%2F04%2F30%2F6599212.jpg&w=272&h=272&c=sc&poi=face&q=60",
+        name: " Carrot Cake Energy Bites ",
+        description: " These no-cook energy bites keep well in the fridge or freezer and are easy to grab on the go for a healthy snack. ",
+        type: {
+            typeName: "snack",
+            id: 4
+        }
+    },
+    {
+        id: 22,
+        imageLink: "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F44%2F2019%2F08%2F26232326%2F6098309.jpg&w=272&h=272&c=sc&poi=face&q=60",
+        name: " Just because you're watching carbs doesn't mean you can't have the classic breakfast pastry. In place of all-purpose flour, nutty almond and coconut flours make these muffins fluffy while slashing carbs. A bit of brown sugar blunts the tartness of fresh blueberries. Make these ahead for a grab-and-go breakfast for the week. ",
+        description: " Low-Carb Blueberry Muffins ",
+        type: {
+            typeName: "snack",
+            id: 4
+        }
+    }, {
+        id: 23,
+        imageLink: "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F44%2F2021%2F05%2F17%2Fcauliflower-chips-with-parmesan.jpg&w=272&h=272&c=sc&poi=%5B940%2C960%5D&q=60&rect=0%2C0%2C2000%2C2000",
+        name: " Cauliflower Chips ",
+        description: " These crisp cauliflower chips are low in carbs and can be made in either the oven or your air fryer. Plus, they require just cauliflower, Parmesan cheese and seasonings to make! Swap out the Italian seasoning for ranch seasoning or your favorite spice blend for a different flavor profile. ",
+        type: {
+            typeName: "snack",
+            id: 4
+        }
+    }, {
+        id: 24,
+        imageLink: "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F44%2F2020%2F05%2F21%2F8149517.jpg&w=272&h=272&c=sc&poi=face&q=60",
+        name: " Air-Fryer Zucchini Chips ",
+        description: " These crispy air-fryer zucchini chips are delicious paired with the creamy dill dipping sauce--or any sauce you have on hand. And the best part? The air fryer delivers a crispy crunch without having to pile on the oil like traditional fryers do. ",
+        type: {
+            typeName: "snack",
+            id: 4
+        }
+    },
+    {
+        id: 25,
+        imageLink: "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F44%2F2017%2F06%2F27113344%2F45362581.jpg&w=272&h=272&c=sc&poi=face&q=60",
+        name: " Strawberry-Chocolate Greek Yogurt Bark ",
+        description: " Lightly sweetened Greek yogurt gets studded with fresh strawberries and chocolate chips then frozen so you can break it into chunks just like chocolate bark (but healthier!). This colorful snack or healthy dessert is perfect for kids and adults alike. Use full-fat yogurt to ensure the creamiest bark possible. ",
+        type: {
+            typeName: "snack",
+            id: 4
+        }
+    },
+
+
+]
+
+export interface dietPlan {
+    imageLink: string;
+    name: string;
+    description: string;
+    id: number;
+    type: {
+        typeName: string,
+        id: number
+    }
+}
